e3fb6561bf535c824145ed821631ea617c451525
Vosthek <alberto93.dev@gmail.com>
2020-05-05 21:24:52 +0200

    FEATURE: prefabloader window and play save mode.

--- MVDUnityTools/Assets/Editor/PrefabLoader.cs
+++ MVDUnityTools/Assets/Editor/PrefabLoader.cs
@@ -5,6 +5,13 @@ using UnityEditor;
 
 public class PrefabLoader : EditorWindow
 {
+    private int width = 8;
+    private float offset = 8f;
+    private Vector2 gridSize;
+
+    private string prefabsPath = "Assets/3DGamekitLite/Art/Models/Characters";
+    private List<string> instances;
+
     // Init function, we create the window and initialize settings
     [MenuItem("MVD/Prefab Loader")]
     static void Init()
@@ -17,6 +24,66 @@ public class PrefabLoader : EditorWindow
     // Method used to draw anything on our window screen.
     void OnGUI()
     {
+        // Canvas drawing elements
+        prefabsPath = EditorGUILayout.TextField("Prefabs path", prefabsPath);
+        width = EditorGUILayout.IntField("Grid width", width);
+        offset = EditorGUILayout.FloatField("Offset", offset);
+        gridSize = EditorGUILayout.Vector2Field("Grid Size", gridSize);
 
+        GUIStyle editorStyle = new GUIStyle(GUI.skin.button);
+        Color myStyleColor = Color.blue;
+        editorStyle.fontStyle = FontStyle.Bold;
+        editorStyle.normal.textColor = myStyleColor;
+
+        if (GUILayout.Button("Build Tilesets", editorStyle))
+        {
+            instances = new List<string>();
+
+            RetrievePrefabs(prefabsPath);
+            SpawnPrefabs();
+        }
+    }
+
+    // Get all the prefabs from the folder
+    void RetrievePrefabs(string path)
+    {
+        string[] prefabs = AssetDatabase.FindAssets("", new string[] { prefabsPath });
+
+        foreach(string assetguid in prefabs)
+        {
+            string assetpath = AssetDatabase.GUIDToAssetPath(assetguid);
+
+            Debug.Log(assetpath);
+
+            if(AssetDatabase.IsValidFolder(assetpath))
+            {
+                //RetrievePrefabs(assetpath);
+                continue;
+            }
+
+            if(!instances.Contains(assetpath))
+                instances.Add(assetpath);
+        }
+    }
+
+    // Spawn the prefabs on the scene
+    void SpawnPrefabs()
+    {
+        int x_gridpos = 0, y_gridpos = 0;
+
+        foreach(string path in instances)
+        {
+            // Get the grid position
+            float pos_x = x_gridpos * (gridSize.x + offset);
+            float pos_y = y_gridpos * (gridSize.y + offset);
+
+            // Update the grid position
+            x_gridpos = (x_gridpos + 1) % width;
+            y_gridpos += x_gridpos % width == 0 ? 1 : 0;
+
+            Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
+            GameObject prefab = PrefabUtility.InstantiatePrefab(obj) as GameObject;
+            prefab.transform.position = new Vector3(pos_x, 0, pos_y);
+        }
     }
 }
--- MVDUnityTools/Assets/Source/MenuSnippets.cs
+++ MVDUnityTools/Assets/Source/MenuSnippets.cs
@@ -3,8 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[InitializeOnLoad]
 public class MenuSnippets
 {
+    // I want to keep trace of everything that i saved
+    private static Dictionary<GameObject, GameObject> changedPrefabs;
+
+    static MenuSnippets()
+    {
+        changedPrefabs = new Dictionary<GameObject, GameObject>();
+        EditorApplication.playmodeStateChanged += OnPlayStateChanged;
+    }
+
+    static void OnPlayStateChanged()
+    {
+        if(!EditorApplication.isPlaying)
+        {
+            foreach(GameObject original in changedPrefabs.Keys)
+            {
+                // We can just go through the object
+                // Change all component values with the ones that we have on the prefab
+                GameObject ctx_copy_object = PrefabUtility.InstantiatePrefab(changedPrefabs[original]) as GameObject;
+
+                Vector3 local_pos = ctx_copy_object.transform.position;
+                PrefabUtility.UnpackPrefabInstance(ctx_copy_object, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+                ctx_copy_object.transform.SetParent(original.transform.parent.transform);
+                ctx_copy_object.transform.name = original.name;
+                ctx_copy_object.transform.SetSiblingIndex(original.transform.GetSiblingIndex());
+                ctx_copy_object.transform.localPosition = local_pos;
+
+                // Method 2, more elegant, but still need work to go.
+                //GameObject ctx_copy_object = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                //ctx_copy_object.hideFlags = HideFlags.HideInHierarchy;
+                //Component[] components = ctx_copy_object.GetComponents(typeof(Component));
+
+                //// Loop through all the components and copy from prefab to gameobject
+                //for (int i = 0; i < components.Length; i++)
+                //{
+                //    System.Type typed = components[i].GetType();
+                //    Component copy_comp = original.GetComponent(typed);
+                //    copy_comp.GetCopyOf(components[i]);
+
+                //    if(typed.Name == "MeshFilter")
+                //    {
+                //        Debug.Log("meshfolter");
+                //    }
+                //}
+
+                UnityEngine.Object.DestroyImmediate(original);
+                string prefab_path = AssetDatabase.GetAssetPath(changedPrefabs[original]);
+                AssetDatabase.DeleteAsset(prefab_path);
+            }
+        }
+    }
+
     [MenuItem("CONTEXT/Transform/Randomize Position")]
     private static void RandomizeTransform(MenuCommand cmd)
     {
@@ -38,4 +90,13 @@ public class MenuSnippets
         }
     }
 
+    [MenuItem("CONTEXT/Transform/Save GameObject")]
+    private static void SaveGameObject(MenuCommand cmd)
+    {
+        // Get the actual gameobject from the context and save it as a prefab
+        GameObject ctx_object = cmd.context as GameObject;
+        GameObject prefab = PrefabUtility.CreatePrefab("Assets/copy_" + ctx_object.name + ".prefab", ctx_object);
+
+        changedPrefabs.Add(ctx_object, prefab);
+    }
 }
