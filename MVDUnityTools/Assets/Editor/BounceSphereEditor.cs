07a3bbe0789cd88f246f9fb1c3c35d703cacd323
Vosthek <alberto93.dev@gmail.com>
2020-05-05 18:51:23 +0200

    FEATURE: Bounce sphere editor script sample.

---
+++ MVDUnityTools/Assets/Editor/BounceSphereEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BounceSphere))]
+public class BounceSphereEditor : Editor
+{
+    // Override the inspector drawing method
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        BounceSphere spherescript = (BounceSphere)target;
+
+        if(GUILayout.Button("Randomize"))
+        {
+            spherescript.health = Random.Range(0, 100);
+        }
+    }
+}
