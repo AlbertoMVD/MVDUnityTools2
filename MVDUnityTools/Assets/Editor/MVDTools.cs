cc8e5f1adac735a672a97a3ad95ee65ebaca4a53
Vosthek <alberto93.dev@gmail.com>
2020-05-12 18:56:11 +0200

    FEATURE: Adding the mvd tools window.

--- MVDUnityTools/Assets/Editor/MVDTools.cs
+++ MVDUnityTools/Assets/Editor/MVDTools.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-public class PrefabLoader : EditorWindow
+public class MVDTools : EditorWindow
 {
     private int width = 8;
     private float offset = 8f;
@@ -12,17 +12,28 @@ public class PrefabLoader : EditorWindow
     private string prefabsPath = "Assets/3DGamekitLite/Art/Models/Characters";
     private List<string> instances;
 
+    private bool[] foldout = new bool[] { false, false, true };
+
     // Init function, we create the window and initialize settings
     [MenuItem("MVD/Prefab Loader")]
     static void Init()
     {
         // Get existing open window or if none, make a new one:
-        PrefabLoader window = (PrefabLoader)EditorWindow.GetWindow(typeof(PrefabLoader));
+        MVDTools window = (MVDTools)EditorWindow.GetWindow(typeof(MVDTools));
         window.Show();
     }
 
+    /* Editor UI METHODS */
+
     // Method used to draw anything on our window screen.
     void OnGUI()
+    {
+        DisplayPanel("Utilities", () => DisplayUtilities(), ref foldout[0]);
+        DisplayPanel("Prefab Loader", () => DisplayPrefabLoader(), ref foldout[1]);
+        DisplayPanel("Prefab Spawner", () => DisplayPrefabSpawner(), ref foldout[2]);
+    }
+
+    private void DisplayPrefabLoader()
     {
         // Canvas drawing elements
         prefabsPath = EditorGUILayout.TextField("Prefabs path", prefabsPath);
@@ -44,24 +55,60 @@ public class PrefabLoader : EditorWindow
         }
     }
 
+    // Method used to display the utility tools.
+    private void DisplayUtilities()
+    {
+        // TO-DO
+        // Implement the UI for the necessary utilities required for the final deliver.
+    }
+
+    // Method used to display the prefab placement tools.
+    private void DisplayPrefabSpawner()
+    {
+
+    }
+
+    static void DisplayPanel(string title, System.Action toexecute, ref bool foldout)
+    {
+        //GUI.color = color;
+        EditorGUILayout.BeginVertical("Box");
+        {
+            GUI.color = Color.white;
+            GUILayout.Space(3);
+            GUILayout.BeginHorizontal();
+            foldout = EditorGUILayout.Foldout(foldout, title, foldout);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(3);
+
+            if (foldout)
+            {
+                // Method to be called.
+                toexecute();
+            }
+        }
+        GUILayout.EndVertical();
+    }
+
+    // Functions to execute functionality
+
     // Get all the prefabs from the folder
     void RetrievePrefabs(string path)
     {
         string[] prefabs = AssetDatabase.FindAssets("", new string[] { prefabsPath });
 
-        foreach(string assetguid in prefabs)
+        foreach (string assetguid in prefabs)
         {
             string assetpath = AssetDatabase.GUIDToAssetPath(assetguid);
 
             Debug.Log(assetpath);
 
-            if(AssetDatabase.IsValidFolder(assetpath))
+            if (AssetDatabase.IsValidFolder(assetpath))
             {
                 //RetrievePrefabs(assetpath);
                 continue;
             }
 
-            if(!instances.Contains(assetpath))
+            if (!instances.Contains(assetpath))
                 instances.Add(assetpath);
         }
     }
@@ -71,7 +118,7 @@ public class PrefabLoader : EditorWindow
     {
         int x_gridpos = 0, y_gridpos = 0;
 
-        foreach(string path in instances)
+        foreach (string path in instances)
         {
             // Get the grid position
             float pos_x = x_gridpos * (gridSize.x + offset);
